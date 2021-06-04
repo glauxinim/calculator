04c3a85d2e0ff1bb0044a6f3a03e25abb241e2df
glauxinim <glau.koba@gmail.com>
2021-06-04 18:41:40 -0300

    Add To-do List
    Html- Css - Js

---
+++ todoScript.js
@@ -0,0 +1,15 @@
+function addText() {
+  if (document.getElementById("textInput").value !== "") {
+    let textInputValue = document.getElementById("textInput").value;
+
+    let newTodoItem = document.createElement("li");
+    newTodoItem.innerHTML = textInputValue;
+    document.getElementById("list").appendChild(newTodoItem);
+
+    document.getElementById("textInput").value = "";
+  }
+}
+
+function removeList() {
+  document.getElementById("list").innerHTML = "";
+}
